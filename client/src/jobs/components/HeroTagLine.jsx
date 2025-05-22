6fc4afafd736fde386f391843972ce475ae31cb7
FedericoPiazzolla <Federicop257@gmail.com>
2025-05-22 16:31:01 +0200

    add: hero component style

---
+++ client/src/jobs/components/HeroTagLine.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style/HeroTagLine.css";
+
+const HeroTagLine = () => {
+  return (
+    <div className="_container">
+      <p className="hero-tagline">
+        Job Tracker simplifies your job hunt
+        <span className="">track, manage, and win every opportunity.</span>
+      </p>
+    </div>
+  );
+};
+
+export default HeroTagLine;
--- client/src/jobs/pages/LandingPage.jsx
+++ client/src/jobs/pages/LandingPage.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Hero from "../components/Hero";
+import HeroTagLine from "../components/HeroTagLine";
 
 const LandingPage = () => {
   return (
     <>
       <div className="landing-page">
         <Hero />
+        <HeroTagLine />
       </div>
     </>
   );
