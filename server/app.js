0b9b6a6981c023c269b306eace0148e171b65cc2
FedericoPiazzolla <Federicop257@gmail.com>
2025-06-02 17:02:08 +0200

    create: job model

--- server/app.js
+++ server/app.js
@@ -3,12 +3,20 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const usersRoutes = require("./routes/users-routes");
+const jobsRoutes = require("./routes/jobs-routes");
+const HttpError = require("./models/http-error");
 
 const app = express();
 
 app.use(bodyParser.json());
 
 app.use("/api/users", usersRoutes);
+app.use("/api/jobs", jobsRoutes);
+
+app.use((req, res, next) => {
+  const error = new HttpError("Could not find this route.", 404);
+  throw error;
+});
 
 mongoose
   .connect(
---
+++ server/models/job.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const placeSchema = new Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  website: { type: String },
+  company: { type: String, required: true },
+  location: { type: String, required: true },
+  status: {
+    type: String,
+    required: true,
+    enum: ["applied", "interview", "offer", "rejected"],
+  },
+  date: { type: Date, required: true },
+  salary: { type: String }, // può essere un range o un valore singolo
+  creator: { type: mongoose.Types.ObjectId, required: true, ref: "User" },
+  // un place può essere creato solo da un utente
+});
+
+module.exports = mongoose.model("Place", placeSchema);
