b5ddf1ad2de40280eb1fd073fca9392ac18b56db
FedericoPiazzolla <Federicop257@gmail.com>
2025-06-04 22:25:09 +0200

    add: deleteJob routes

--- server/controllers/jobs-controller.js
+++ server/controllers/jobs-controller.js
@@ -110,6 +110,103 @@ const createJob = async (req, res, next) => {
   res.status(201).json({ job: createdJob });
 };
 
+const updateJobById = async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return next(
+      new HttpError("Invalid inputs passed, please check your data.", 422)
+    );
+  }
+
+  const {
+    title,
+    description,
+    website,
+    company,
+    location,
+    status,
+    date,
+    salary,
+  } = req.body;
+  const jobId = req.params.jid;
+
+  let job;
+  try {
+    job = await Job.findById(jobId);
+  } catch (err) {
+    const error = new HttpError(
+      "Something went wrong, could not update job.",
+      500
+    );
+    return next(error);
+  }
+
+  if (job.creator.toString() !== req.userData.userId) {
+    const error = new HttpError("You are not allowed to edit this job.", 401);
+    return next(error);
+  }
+  job.title = title;
+  job.description = description;
+  job.website = website;
+  job.company = company;
+  job.location = location;
+  job.status = status;
+  job.date = new Date(date);
+  job.salary = salary;
+  try {
+    await job.save();
+  } catch (err) {
+    const error = new HttpError(
+      "Something went wrong, could not update job.",
+      500
+    );
+    return next(error);
+  }
+  res.status(200).json({ job: job.toObject({ getters: true }) });
+};
+
+const deleteJob = async (req, res, next) => {
+  const jobId = req.params.jid;
+  let job;
+  try {
+    job = await Job.findById(jobId).populate("creator");
+  } catch (err) {
+    const error = new HttpError(
+      "Something went wrong, could not delete job.",
+      500
+    );
+    return next(error);
+  }
+
+  if (!job) {
+    const error = new HttpError("Could not find job for this id.", 404);
+    return next(error);
+  }
+
+  if (job.creator.id !== req.userData.userId) {
+    const error = new HttpError("You are not allowed to delete this job.", 401);
+    return next(error);
+  }
+
+  try {
+    const sess = await mongoose.startSession();
+    sess.startTransaction();
+    await job.remove({ session: sess });
+    job.creator.jobs.pull(job);
+    await job.creator.save({ session: sess });
+    await sess.commitTransaction();
+  } catch (err) {
+    const error = new HttpError(
+      "Something went wrong, could not delete job.",
+      500
+    );
+    return next(error);
+  }
+
+  res.status(200).json({ message: "Deleted job." });
+};
 exports.getJobById = getJobById;
 exports.getJobsByUserId = getJobsByUserId;
 exports.createJob = createJob;
+exports.updateJobById = updateJobById;
+exports.deleteJob = deleteJob;
